800fdc18f60ddb836a8ee1c170a096fbd4194882
fery dwi ramadhi <ferydwir27@gmail.com>
2026-08-19 20:46:19 +0700

    Add profile model and profil migration

---
+++ backend/database/migrations/2026_08_10_073320_profil.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        //
+        Schema::create('profile',function(Blueprint $table){
+            $table->id();
+            $table->string('name');
+            $table->text('about')->nullable();
+            $table->string('alamat')->nullable();
+            $table->string('no_telp')->nullable();
+            $table->string('email')->nullable();
+            $table->string('photo')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        //
+        Schema::dropIfExists('profile');
+    }
+};
